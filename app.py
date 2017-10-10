f417292fb8a7d5977459406fdd1557d4d7233fbf
Ziqiang Feng <fengziqiang@hotmail.com>
2017-10-10 02:27:28 -0500

    2

--- app.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Date    : 2017-10-10 11:32:24
-# @Author  : Your Name (you@example.org)
-# @Link    : http://example.org
-# @Version : $Id$
-
-import os
